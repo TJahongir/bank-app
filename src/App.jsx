1b128613095aa517d947424b761dbf69a2134dc8
TJahongir <jakhongirt@gmail.com>
2022-12-28 23:59:38 +0500

    111

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import styles from "./style";
+import { `styles `} from "./style";
 
 const App = () => {
 	return (
 		<div className="bg-primary w-full overflow-hidden">
 			<div className={`${styles.paddingX} ${styles.flexCenter} `}>
-				<div className={`${styles.boxWidth}`}>Navbar</div>
+				<div className={`${styles.boxWidth}`}></div>
 			</div>
 		</div>
 	);
